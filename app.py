0b814a909bbd6481bd34d68cde3325456d7b7211
Steffin Issac <steffinissac@gmail.com>
2026-01-28 06:52:01 -0600

    Add the app.py file

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+# Our "database" - just a list in memory for now
+todos = []
+
+@app.route('/')
+def home():
+    return "Todo API is running!"
+
+@app.route('/todos', methods=['GET'])
+def get_todos():
+    return jsonify(todos)
+
+@app.route('/todos', methods=['POST'])
+def add_todo():
+    data = request.get_json()
+    todo = {
+        'id': len(todos) + 1,
+        'task': data['task'],
+        'done': False
+    }
+    todos.append(todo)
+    return jsonify(todo), 201
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
